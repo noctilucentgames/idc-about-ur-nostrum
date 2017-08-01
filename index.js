f5ef9f61303df3a374dc796d42e716840f551bd2
noctilucentgames <noctilucentgames@gmail.com>
2017-08-01 15:56:48 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function idcAboutUrNostrum(dispatch){
+    const ploc = new (require('../../cat_utils/playerLocation.js'))(dispatch)  
+    const abnHook = (event) => {
+        if ([860033,860034].indexOf(event.id)>-1 && [9860,9511,9777].indexOf(ploc.getTopo())>-1){
+            return false
+        }
+    }
+    dispatch.hook('S_ABNORMALITY_BEGIN', 1, abnHook)
+    dispatch.hook('S_ABNORMALITY_END', 1, abnHook)
+}
