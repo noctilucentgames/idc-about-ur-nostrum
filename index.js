c4c61ae363e5e51e1c7ffac00336cf0a691d2b55
noctilucentgames <noctilucentgames@gmail.com>
2017-08-05 00:50:34 +0200

    merge

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 module.exports = function idcAboutUrNostrum(dispatch){
-<<<<<<< HEAD
     const ploc = new (require('./lib/playerLocation.js'))(dispatch)  
-=======
-    const ploc = new (require('../../cat_utils/playerLocation.js'))(dispatch)  
->>>>>>> 11ccbb6f7eb4b22593a56609d1cf367fd024d654
     const abnHook = (event) => {
         if ([860033,860034].indexOf(event.id)>-1 && [9860,9511,9777].indexOf(ploc.getTopo())>-1){
             return false
